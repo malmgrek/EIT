06e13183f797ceb32aeca371b507129f53342db1
Stratos Staboulis <stratos.staboulis@leanheat.com>
2019-09-06 14:15:40 +0300

    Preliminary support for Netgen meshing

--- src/meshing.jl
+++ src/meshing.jl
@@ -25,3 +25,23 @@ function load_mesh(file)
     )
     return from_dict(dict)
 end
+
+
+function run_netgen(
+    geofile; volfile=nothing, netgen_exec="/opt/netgen/bin/netgen"
+    )
+    # TODO: Add ng_options
+    # TODO: Try to make with minimal side effects
+
+    default_volfile = (
+        (x -> x * ".vol") ∘ 
+        (x -> split(x, "/")[end]) ∘ 
+        (x -> split(x, ".geo")[1])
+    )
+
+    volfile = isnothing(volfile) ? default_volfile(geofile) : volfile
+    @info "Running netgen with $(geofile)"
+    run(`$(netgen_exec) -batchmode -geofile=$(geofile) -meshfile=$(volfile)`)
+    @info "Wrote mesh to $(volfile)"
+    return volfile
+end
